86cdad58541653573942ff09fd38f7b44ff51139
liamflynn2003 <20098690@mail.wit.ie>
2024-09-14 15:45:57 +0100

    Complete type safety to colleagues data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -11,6 +11,10 @@ interface Colleague {
         extension: number
     }
 }
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+}
 const friend1: Friend = {
     name: "Paul Fleming",
     phone: "087-12345",
@@ -53,7 +57,7 @@ const friend1: Friend = {
       extension: 125,
     },
   };
-  const colleagues = {
+  export const colleagues : ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
