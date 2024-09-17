a11ef3a0b1feb18f9c9ff97132c5fa5e76f584d3
liamflynn2003 <20098690@mail.wit.ie>
2024-09-17 10:10:30 +0100

    Functions with generics.

---
+++ src/04-generics.ts
@@ -0,0 +1,24 @@
+import {friends, colleagues} from './01-basics'
+import {Friend, Colleague} from './myTypes'
+
+function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined {
+    return data.find((criteria))
+}
+
+// console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')  ))
+// console.log(findMatch<Colleague>(colleagues.current, (c) => c.department === 'Finance'  ))
+
+function sort<T>( data : T[], criteria: (d1: T,d2: T) => number ): T[] {
+    const result = data.sort(criteria);
+    return result
+}
+
+// Sort friends by age
+console.log(sort<Friend>(friends, (a, b) => a.age - b.age));
+// Sort colleagues by extension number
+console.log(
+  sort<Colleague>(
+    colleagues.current,
+    (a, b) => a.contact.extension - b.contact.extension
+  )
+);
